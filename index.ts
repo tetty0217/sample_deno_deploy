37a30cfe65854585fc4f7b0096fc30fd91cbb76b
Tetta Mori <mori-tetta@dmm.com>
2021-03-31 01:46:56 +0900

    commit 6

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-function handleRequest(request) {
-  const { pathname } = new URL(request.url);
-
-  // Respond with HTML
-  if (pathname.startsWith("/html")) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
+addEventListener("fetch", (event) => {
+    const html = `<html> 
+        <p style="text-align:center;">
+          <img src="https://avatars.githubusercontent.com/u/23420162?v=4" alt="tetty" width="300" height="300" />
+        </p>
       </html>`;
-
-    return new Response(html, {
+    
+    const res = new Response(html, {
       headers: {
         // The "text/html" part implies to the client that the content is HTML
         // and the "charset=UTF-8" part implies to the client that the content
@@ -15,23 +13,6 @@ function handleRequest(request) {
         "content-type": "text/html; charset=UTF-8",
       },
     });
-  }
 
-  // Respond with JSON
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-}
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+    event.respondWith(res);
 });
