461000d2c6326a4c128c9f17071740b01d32ec7d
Tetta Mori <mori-tetta@dmm.com>
2021-03-31 01:29:37 +0900

    commit 4

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-alert("Denoだあああああああああああああああ");
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response("Hello world", {
+      status: 200,
+      headers: {
+        "content-type": "text/plain",
+      },
+    }),
+  );
+});
